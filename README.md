b3a1dafa42c6f0ecfda585e7e26d2d67c3356470
ouyangduning <ouyangduning@jd.com>
2020-04-23 19:36:51 +0800

    feat:travis

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+[![codecov](https://codecov.io/gh/a252937166/help-test/branch/master/graph/badge.svg)](https://codecov.io/gh/a252937166/help-test)
+
+
 # help-test
--- src/test/java/com/ouyanglol/helptest/HelpTestApplicationTests.java
+++ src/test/java/com/ouyanglol/helptest/HelpTestApplicationTests.java
@@ -1,26 +1,15 @@
 package com.ouyanglol.helptest;
 
 import com.ouyanglol.helptest.annotation.EnableMock;
-import com.ouyanglol.helptest.annotation.HelpMockBean;
-import com.ouyanglol.helptest.mock.TestMock;
-import com.ouyanglol.helptest.service.TestService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @EnableMock
 class HelpTestApplicationTests {
 
-    @HelpMockBean(beanClass = TestService.class)
-    private TestMock testMock;
-
-    @Autowired
-    private TestService testService;
-
     @Test
     void contextLoads() {
-        System.out.println(testService.test());
     }
 
 }
